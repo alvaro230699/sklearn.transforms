7d8ec5a4cbb4d91ca70d1fb3a9cd4b221339c47c
alvaro230699 <49374211+alvaro230699@users.noreply.github.com>
2020-12-05 13:25:04 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,15 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+class Arreglarkoi_score(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data=X.copy()
+        if 'koi_pdisposition' in data.columns.values:
+            data['koi_score'][data['koi_pdisposition']=='FALSE POSITIVE']=-1*X[X['koi_pdisposition']=='FALSE POSITIVE']['koi_score']     
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data
